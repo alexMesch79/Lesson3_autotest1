2c84bda85504a17a6acb9c438fa7a6fa00c97046
AlexeyM <alex_mesch@mail.ru>
2022-05-31 11:43:19 +0300

    refactoring check-box and file way

--- src/test/java/tests/PracticeformTest.java
+++ src/test/java/tests/PracticeformTest.java
@@ -1,14 +1,11 @@
-package test;
+package tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import java.io.File;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
@@ -25,26 +22,26 @@ public class PracticeformTest {
         executeJavaScript("$('footer').remove()");
         executeJavaScript("$('#fixedban').remove()");
 
-        $("[id=firstName]").setValue("Vladimir");
-        $("[id=lastName]").setValue("Ulyanov");
-        $("[id=userEmail]").setValue("Lenin@yandex.ru");
-        $("[for=gender-radio-1]").click();
-        $("[id=userNumber]").setValue("9273731234");
-        $("[id=dateOfBirthInput]").click();
+        $("#firstName").setValue("Vladimir");
+        $("#lastName").setValue("Ulyanov");
+        $("#userEmail").setValue("Lenin@yandex.ru");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("9273731234");
+        $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("April");
         $(".react-datepicker__year-select").selectOption("1935");
         $("[aria-label='Choose Wednesday, April 3rd, 1935']").click();
-        $("[id=subjectsInput]").sendKeys("Arts");
-        $("[id=subjectsInput]").pressEnter();
-        $("[for=hobbies-checkbox-1]").click();
-        $("[for=hobbies-checkbox-2]").click();
-        $("[for=hobbies-checkbox-3]").click();
-        $("[id=uploadPicture]").uploadFile(new File("src/main/java/resources/Picture.jpg"));
-        $("[id=currentAddress]").setValue("RF, Moscow City, Red Area, House 1");
-        $(byText("Select State")).click();
-        $(withText("Haryana")).click();
-        $(byText("Select City")).click();
-        $(withText("Panipat")).click();
+        $("#subjectsInput").sendKeys("Arts");
+        $("#subjectsInput").pressEnter();
+        $(byText("Sports")).click();
+        $(byText("Reading")).click();
+        $(byText("Music")).click();
+        $("[id=uploadPicture]").uploadFile(new File("src/test/resources/Picture.jpg"));
+        $("#currentAddress").setValue("RF, Moscow City, Red Area, House 1");
+        $("#state").click();
+        $(byText("Haryana")).click();
+        $("#city").click();
+        $(byText("Panipat")).click();
         $("[id=submit]").click();
 
         $(".modal-body").shouldHave(
