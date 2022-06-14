0db246721e381d1637577a00a821fa86e557ea27
AlexeyM <alex_mesch@mail.ru>
2022-06-14 23:10:12 +0300

    added components City and Stat

--- src/test/java/pages/components/InputCom.java
+++ src/test/java/pages/components/InputCom.java
@@ -2,15 +2,7 @@ package pages.components;
 
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Selenide.$;
-
 public class InputCom {
-    /*
-    public void SetValue(String selector, String value) {
-
-        $(selector).setValue(value).pressEnter();
-    }
-    */
 
     public void SetValue(SelenideElement selector, String value) {
 
